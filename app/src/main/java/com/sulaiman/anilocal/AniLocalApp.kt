d5dfc9a71b8341ab528b9a339ad92e13821a90f8
Sulaimansu <Solvo477@gmail.com>
2026-04-11 02:58:55 +0300

    fixing crashes

--- app/src/main/java/com/sulaiman/anilocal/AniLocalApp.kt
+++ app/src/main/java/com/sulaiman/anilocal/AniLocalApp.kt
@@ -1,15 +1,22 @@
 package com.sulaiman.anilocal
 
 import android.app.Application
+import androidx.hilt.work.HiltWorkerFactory
 import androidx.work.Configuration
 import androidx.work.WorkManager
 import com.sulaiman.anilocal.worker.AiringSyncWorker
 import dagger.hilt.android.HiltAndroidApp
+import javax.inject.Inject
 
 @HiltAndroidApp
 class AniLocalApp : Application(), Configuration.Provider {
+
+    @Inject
+    lateinit var workerFactory: HiltWorkerFactory
+
     override val workManagerConfiguration: Configuration
         get() = Configuration.Builder()
+            .setWorkerFactory(workerFactory)
             .setDefaultProcessName("${BuildConfig.APPLICATION_ID}:bg")
             .build()
 
